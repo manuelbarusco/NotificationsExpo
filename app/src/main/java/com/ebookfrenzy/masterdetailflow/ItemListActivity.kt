bd347561fe830eac54c1a2d9375bf951abe46fde
manuelbarusco <barusco.manuel@gmail.com>
2021-05-17 12:30:35 +0200

    Inserita parte iniziale di Simone

---
+++ app/src/main/java/com/ebookfrenzy/masterdetailflow/ItemListActivity.kt
@@ -0,0 +1,128 @@
+package com.ebookfrenzy.masterdetailflow
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.RecyclerView
+import com.ebookfrenzy.masterdetailflow.dummy.DummyContent
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * An activity representing a list of Pings. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of ITEMS, which when touched,
+ * lead to a [ItemDetailActivity] representing
+ * item details. On tablets, the activity presents the list of ITEMS and
+ * item details side-by-side using two vertical panes.
+ */
+class ItemListActivity : AppCompatActivity() {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private var twoPane: Boolean = false
+    //private val d: DummyContent = DummyContent(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /*val images_id: Int = resources.getIdentifier("image_chat1","drawable", "com.ebookfrenzy.masterdetailflow")
+        Log.d("Debug","id immagine: $images_id invece di ${R.drawable.image_chat1}")*/
+
+        setContentView(R.layout.activity_item_list)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.title = title
+
+        //Gestione FloatingActionButton
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            //se toccato compare da sotto uno snackbar
+        }
+
+        if (findViewById<NestedScrollView>(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            twoPane = true
+        }
+
+        setupRecyclerView(findViewById(R.id.item_list))
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, twoPane)
+    }
+
+    class SimpleItemRecyclerViewAdapter(private val parentActivity: ItemListActivity,
+                                        private val values: List<DummyContent.DummyItem>,
+                                        private val twoPane: Boolean) :
+            RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
+
+        private val onClickListener: View.OnClickListener
+        //click listener per l'elemento della lista
+        init {
+            onClickListener = View.OnClickListener { v ->
+                val item = v.tag as DummyContent.DummyItem
+                if (twoPane) {
+                    val fragment = ItemDetailFragment().apply {
+                        arguments = Bundle().apply {
+                            //putString(ItemDetailFragment.ARG_ITEM_ID, item.id)      //passare id chat
+                        }
+                    }
+                    parentActivity.supportFragmentManager
+                            .beginTransaction()
+                            .replace(R .id.item_detail_container, fragment)
+                            .commit()
+                } else {
+                    val intent = Intent(v.context, ItemDetailActivity::class.java).apply {
+                        //putExtra(ItemDetailFragment.ARG_ITEM_ID, item.id)      //passare id chat
+                    }
+                    v.context.startActivity(intent)
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_list_content, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = values[position]
+            val id_im = item.imageId
+            holder.image.setImageResource(id_im)
+            holder.name.text = item.name
+            holder.notification.text = item.notification
+            holder.time.text = item.time
+
+            with(holder.itemView) {
+                tag = item
+                //imposto il listener scritto sopra
+                setOnClickListener(onClickListener)
+            }
+        }
+
+        override fun getItemCount() = values.size
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val name: TextView = view.findViewById(R.id.name)
+            val notification: TextView = view.findViewById(R.id.notication)
+            val time: TextView = view.findViewById(R.id.time)
+            val image: ImageView = view.findViewById(R.id.image)
+        }
+    }
+}
